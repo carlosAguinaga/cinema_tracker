a06deff0599484fd940badc5e8d8683a2464747a
Carlos <carlosaguinaga01@gmail.com>
2024-01-10 21:54:47 -0500

    search delegate added

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,10 +1,15 @@
+import 'package:cinema_tracker/domain/entities/movie.dart';
+import 'package:cinema_tracker/presentation/delegates/search_movie_delegate.dart';
+import 'package:cinema_tracker/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
-class CustomAppbar extends StatelessWidget {
+class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
 
@@ -23,7 +28,28 @@ class CustomAppbar extends StatelessWidget {
                 const SizedBox(width: 5),
                 Text('Cinemapedia', style: titleStyle),
                 const Spacer(),
-                IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+                IconButton(
+                  onPressed: () {
+                    // final movieRepository = ref.read(movieRepositoryProvider);
+                    final searchedMovies = ref.read(searchedMoviesProvider);
+                    final searchQuery = ref.read(searchQueryProvider);
+
+                    showSearch<Movie?>(
+                      query: searchQuery,
+                      context: context,
+                      delegate: SearchMovieDelegate(
+                        initialMovies: searchedMovies,
+                        searchMovies: ref
+                            .read(searchedMoviesProvider.notifier)
+                            .searchMovieByQuery,
+                      ),
+                    ).then((movie) {
+                      if (movie == null) return;
+                      context.push('/movie/${movie.id}');
+                    });
+                  },
+                  icon: const Icon(Icons.search),
+                )
               ],
             ),
           ),
